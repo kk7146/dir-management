81a5dc000dc3ad615598269a2ffa4f3078ece7bf
kk7146 <kk21322132@icloud.com>
2024-11-21 16:46:12 +0900

    feat:ls_resolve

--- libcmd/cmd_ls.c
+++ libcmd/cmd_ls.c
@@ -34,16 +34,6 @@ static int ls_func(int a_flag, int l_flag, int s_flag, int r_flag){
 
     // 디렉토리 엔트리 순회
     while ((entry = readdir(dir)) != NULL) {
-        if (l_flag) {
-            print_permissions(statbuf.st_mode);
-            printf(" %ld ", (long)statbuf.st_nlink);
-            pw = getpwuid(statbuf.st_uid);
-            gr = getgrgid(statbuf.st_gid);
-            printf("%s %s ", pw->pw_name, gr->gr_name);
-            printf("%5lld ", (long long)statbuf.st_size);
-            strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", localtime(&statbuf.st_mtime));
-            printf("%s ", timebuf);
-        }
         if (!a_flag && entry->d_name[0] == '.') {
             continue;  // -a 옵션이 없는 경우 숨김 파일 무시
         }
@@ -55,6 +45,16 @@ static int ls_func(int a_flag, int l_flag, int s_flag, int r_flag){
         }
 
         // 상세 정보 표시 (-l 옵션)
+        if (l_flag) {
+            print_permissions(statbuf.st_mode);
+            printf(" %ld ", (long)statbuf.st_nlink);
+            pw = getpwuid(statbuf.st_uid);
+            gr = getgrgid(statbuf.st_gid);
+            printf("%s %s ", pw->pw_name, gr->gr_name);
+            printf("%5lld ", (long long)statbuf.st_size);
+            strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", localtime(&statbuf.st_mtime));
+            printf("%s ", timebuf);
+        }
         printf("%s", entry->d_name);
         // 심볼릭 링크 대상 표시
         if (S_ISLNK(statbuf.st_mode)) {
@@ -79,6 +79,7 @@ int cmd_ls(int argc, char **argv) {
     int r_flag = 0;      // -r 옵션 플래그 (역순 정렬)
 
     // getopt를 사용하여 옵션 파싱
+    optind = 1;
     while ((opt = getopt(argc, argv, "altr")) != -1) {
         switch (opt) {
             case 'a':
