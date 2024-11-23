575c36de9bd3f6f93d02f407940da3c88982eea7
kk7146 <kk21322132@icloud.com>
2024-11-23 14:17:29 +0900

    test

--- srcs/customshell.c
+++ srcs/customshell.c
@@ -83,7 +83,6 @@ int execute_shell(char *command, cmd_node *const head) {
             else
                 printf("%s: command not found\n", input_argv[0]);
         }
-        optreset = 1;
         optind = 1;
     }
     return -1;
